13470e459ff8a5fe494177dd5df8d2b2dc4799a6
DovydasN <klimpau741@gmail.com>
2024-07-25 22:08:55 +0300

    more tasks complited

--- extra tasks arrays/script3.js
+++ extra tasks arrays/script3.js
@@ -117,41 +117,203 @@ console.log('task322')
 console.log(task322)
 
 // 3.23. Gauti tik skaičius, kurie yra didesni už 555.
+let task323 = arr.filter((array) => array > 555)
+console.log('task323')
+console.log(task323)
+
 // 3.24. Gauti tik skaičius, kurie yra didesni arba lygūs 888.
+let task324 = arr.filter((array) => array >= 888)
+console.log('task324')
+console.log(task324)
+
 // 3.25. Gauti tik skaičius, kurie yra didesni arba lygūs 6789.
+let task325 = arr.filter((array) => array >= 6789)
+console.log('task325')
+console.log(task325)
+
 // 3.26. Gauti tik skaičius, kurie yra mažesni už 50.
+let task326 = arr.filter((array) => array < 50)
+console.log('task326')
+console.log(task326)
+
 // 3.27. Gauti tik skaičius, kurie yra mažesni už 1000.
+let task327 = arr.filter((array) => array < 100)
+console.log('task327')
+console.log(task327)
+
 // 3.28. Gauti tik skaičius, kurie yra mažesni arba lygūs -1.
+let task328 = arr.filter((array) => array <= -1)
+console.log('task328')
+console.log(task328)
+
 // 3.29. Gauti tik skaičius, kurie yra mažesni arba lygūs -5564.
+let task329 = arr.filter((array) => array <= -5564)
+console.log('task329')
+console.log(task329)
+
 // 3.30. Gauti tik skaičius, kurie yra mažesni už 1000 ir didesni už 500.
+let task330 = arr.filter((array) => array < 1000 && array > 500)
+console.log('task330')
+console.log(task330)
+
 // 3.31. Gauti tik skaičius, kurie yra mažesni už 100 ir didesni už 0.
+let task331 = arr.filter((array) => array < 100 && array > 0)
+console.log('task331')
+console.log(task331)
+
 // 3.32. Gauti tik skaičius, kurie yra mažesni už 0 ir didesni už -50.
+let task332 = arr.filter((array) => array < 0 && array > -50)
+console.log('task332')
+console.log(task332)
+
 // 3.33. Gauti tik skaičius, kurie yra mažesni arba lygūs 0 ir didesni už -100.
+let task333 = arr.filter((array) => array <= 0 && array > -100)
+console.log('task333')
+console.log(task333)
+
 // 3.34. Gauti tik skaičius, kurie yra didesni arba lygūs 0 ir mažesni už 55.
+let task334 = arr.filter((array) => array >= 0 && array < 55)
+console.log('task334')
+console.log(task334)
+
 // 3.35. Gauti tik skaičius, kurie yra didesni arba lygūs 444 ir mažesni arba lygūs 654.
+let task335 = arr.filter((array) => array >= 444 && array < 654)
+console.log('task335')
+console.log(task335)
+
 // 3.36. Gauti tik teigiamus skaičius, kurie dalinasi iš 2.
+let task336 = arr.filter((array) => array >= 0 &&  array % 2 === 0)
+console.log('task336')
+console.log(task336)
+
 // 3.37. Gauti tik teigiamus skaičius, kurie dalinasi iš 3.
+let task337 = arr.filter((array) => array >= 0 &&  array % 3 === 0)
+console.log('task337')
+console.log(task337)
+
 // 3.38. Gauti tik neigiamus skaičius, kurie dalinasi iš 4.
+let task338 = arr.filter((array) => array < 0 &&  array % 4 === 0)
+console.log('task338')
+console.log(task338)
+
 // 3.39. Gauti tik neigiamus skaičius, kurie dalinasi iš 111.
+let task339 = arr.filter((array) => array < 0 &&  array % 111 === 0)
+console.log('task339')
+console.log(task339)
+
 // 3.40. Gauti tik skaičius, kurie yra didesni už 500 ir kurie dalinasi iš 2.
+let task340 = arr.filter((array) => array > 500 &&  array % 2 === 0)
+console.log('task340')
+console.log(task340)
+
 // 3.41. Gauti tik skaičius, kurie yra didesni už 1000 ir kurie dalinasi iš 3.
+let task341 = arr.filter((array) => array > 1000 &&  array % 3 === 0)
+console.log('task341')
+console.log(task341)
+
 // 3.42. Gauti tik skaičius, kurie yra mažesni už 1000 ir kurie dalinasi iš 9.
+let task342 = arr.filter((array) => array < 1000 &&  array % 9 === 0)
+console.log('task342')
+console.log(task342)
+
 // 3.43. Gauti tik skaičius, kurie yra mažesni už 500 ir kurie dalinasi iš 2.
+let task343 = arr.filter((array) => array < 500 &&  array % 2 === 0)
+console.log('task343')
+console.log(task343)
+
 // 3.44. Gauti tik skaičius, kurie yra didesni arba lygūs 33 ir kurie dalinasi iš 3.
+let task344 = arr.filter((array) => array >= 33 &&  array % 3 === 0)
+console.log('task344')
+console.log(task344)
+
 // 3.45. Gauti tik skaičius, kurie yra didesni arba lygūs 444 ir kurie dalinasi iš 12.
+let task345 = arr.filter((array) => array >= 444 &&  array % 12 === 0)
+console.log('task345')
+console.log(task345)
+
 // 3.46. Gauti tik skaičius, kurie yra mažesni arba lygūs 155 ir kurie dalinasi iš 5.
+let task346 = arr.filter((array) => array <= 155 &&  array % 5 === 0)
+console.log('task346')
+console.log(task346)
+
 // 3.47. Gauti tik skaičius, kurie yra mažesni arba lygūs -333 ir kurie dalinasi iš 9.
+let task347 = arr.filter((array) => array <= -333 &&  array % 9 === 0)
+console.log('task347')
+console.log(task347)
+
 // 3.48. Gauti tik skaičius, kurie yra didesni už 100, mažesni už 500 ir kurie dalinasi iš 5.
+let task348 = arr.filter((array) => array > 100 && array < 500 &&  array % 5 === 0)
+console.log('task348')
+console.log(task348)
+
 // 3.49. Gauti tik skaičius, kurie yra didesni arba lygūs 888, mažesni už 1000 ir kurie dalinasi iš 2.
+let task349 = arr.filter((array) => array >= 888 && array < 1000 &&  array % 2 === 0)
+console.log('task349')
+console.log(task349)
+
 // 3.50. Gauti tik skaičius, kurie yra mažesni arba lygūs 888, didesni arba lygūs -333, dalinasi iš 3 ir nėra nulis.
+let task350 = arr.filter((array) => array <= 888 && array >= -333 &&  array % 3 === 0 && array !== 0)
+console.log('task350')
+console.log(task350)
+
 // 3.51. Gauti tik skaičius.
+let task351 = arr.filter((array) => typeof array === 'number')
+console.log('task351')
+console.log(task351)
+
 // 3.52. Gauti tik tekstus (string).
+let task352 = arr.filter((array) => typeof array !== 'number')
+console.log('task352')
+console.log(task352)
+
 // 3.53. Gauti tik tekstus (string), kurie turi daugiau nei 5 simbolius.
+let task353 = arr.filter((array) => array.length > 5)
+console.log('task353')
+console.log(task353)
+
 // 3.54. Gauti tik tekstus (string), kurie turi mažiau arba lygiai 5 simbolius.
+let task354 = arr.filter((array) => array.length > 5)
+console.log('task354')
+console.log(task354)
+
 // 3.55. Gauti tik tekstus (string), kurie turi mažiau nei 7 simbolius.
+let task355 = arr.filter((array) => array.length < 7)
+console.log('task355')
+console.log(task355)
+
 // 3.56. Gauti tik tekstus (string), kurie turi raidę t.
+let task356 = arr.filter((array) =>{
+    if (typeof array === 'string'){
+        let findT = array.includes('t')
+        return findT
+    }
+    return false
+} )
+console.log('task356')
+console.log(task356)
+
 // 3.57. Gauti tik tekstus (string), kurie turi raidę y.
+let task357 = arr.filter((array) =>{
+    if (typeof array === 'string'){
+        let findY = array.includes('y')
+        return findY
+    }
+    return false
+} )
+console.log('task357')
+console.log(task357)
+
 // 3.58. Gauti tik tekstus (string), kurie turi raides e arba a.
+let task358 = arr.filter((array) =>{
+    if (typeof array === 'string'){
+        let findEa = array.includes('e', 'a')
+        return findEa
+    }
+    return false
+} )
+console.log('task358')
+console.log(task358)
+
 // 3.59. Gauti tik tekstus (string), kurie turi raides t ir i.
 // 3.60. Gauti tik tekstus (string), kurie turi raide t, bet neturi raidės k.
 // 3.61. Gauti tik tekstus (string), kurie turi raide a, bet neturi raidės s.
