8a3f5d204363357d93cf1c769b58b194f01e6836
DovydasN <klimpau741@gmail.com>
2024-08-16 00:40:24 +0300

    Weather site almost almost done

--- weather Site/script.js
+++ weather Site/script.js
@@ -11,13 +11,6 @@ findCity.addEventListener('submit', (event) =>{
     })
 
    
-   
-    // fetch(`http://api.weatherbit.io/v2.0/current?key=cfef4754224d48a2b3f7b1cd3e8bad7d&city=${city}`)
-    // .then(res => res.json())
-    // .then(data =>{
-        
-    // })
-
 })
 
 function getCity(city) {
@@ -29,7 +22,7 @@ function getCity(city) {
             .then(data => {
                 console.log(data)
                 const cityName = data.data[0].city_name
-                resolve(`Weather in ${cityName}`)
+                resolve(`Weather in ${cityName}, ` + data.data[0].country_code)
                 const tempPar = document.querySelector('#temp')
                 tempPar.textContent = data.data[0].temp + '°C'
                 const sky = document.querySelector('#sky')
@@ -41,9 +34,56 @@ function getCity(city) {
                 windSpd.textContent = `Wind speed: ` + data.data[0].wind_spd
                 const humidity = document.querySelector('#humidity')
                 humidity.textContent = `Humidity: ` + data.data[0].rh
+                const sunRise = document.querySelector('#rise')
+                sunRise.textContent = `Sunrise: ` + data.data[0].sunrise
+                const sunSet = document.querySelector('#set')
+                sunSet.textContent = `Sunset: ` + data.data[0].sunset
             })
     })
 
     return output
 }
 
+function fetchWeatherData(latitude, longitude){
+    fetch(`https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=cfef4754224d48a2b3f7b1cd3e8bad7d`)
+    .then(res => res.json())
+    .then(data => {
+
+        const sunrise = data.data[0].sunrise
+        const sunset = data.data[0].sunset
+        updateBackgroundImage(sunrise, sunset)
+    })
+}
+
+function updateBackgroundImage(sunrise, sunset) {
+
+const now = new Date()
+
+const sunriseTime = new Date(now.toDateString() + ' ' + sunrise)
+const sunsetTime = new Date(now.toDateString() + ' ' + sunset)
+
+
+if (now >= sunriseTime && now < sunsetTime) {
+
+    document.body.style.backgroundImage = 'url(img/cloud-shapes-with-blue-background.jpg)'
+} else {
+
+    document.body.style.backgroundImage = 'url(img/top-view-grey-clouds-arrangement.jpg)'
+}
+
+
+}
+function getLocationAndFetchWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = position.coords.latitude
+                const longitude = position.coords.longitude
+                console.log('Latitude:', latitude)
+                console.log('Longitude:', longitude)
+
+                fetchWeatherData(latitude, longitude)
+            })
+    }
+}
+getLocationAndFetchWeather()
