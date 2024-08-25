7718c44c481cfc84f1f05cbdc2cd2184fe395fc8
DovydasN <klimpau741@gmail.com>
2024-08-25 21:35:49 +0300

    search page almot done

--- API praktika/js/createForm.js
+++ API praktika/js/createForm.js
@@ -12,13 +12,25 @@ export default function createForm() {
 
     const selectElementOption1 = document.createElement('option')
     selectElementOption1.value = 'posts'
-    selectElementOption1.textContent = 'posts'
+    selectElementOption1.textContent = 'Posts'
 
     const selectElementOption2 = document.createElement('option')
     selectElementOption2.value = 'users'
-    selectElementOption2.textContent = 'users'
+    selectElementOption2.textContent = 'Users'
 
-    selectElement.append(selectElementOption, selectElementOption1, selectElementOption2)
+    const selectElementOption3 = document.createElement('option')
+    selectElementOption3.value = 'comments'
+    selectElementOption3.textContent = 'Comments'
+
+    const selectElementOption4 = document.createElement('option')
+    selectElementOption4.value = 'albums'
+    selectElementOption4.textContent = 'Albums'
+
+    const selectElementOption5 = document.createElement('option')
+    selectElementOption5.value = 'photos'
+    selectElementOption5.textContent = 'Photos'
+
+    selectElement.append(selectElementOption, selectElementOption1, selectElementOption2, selectElementOption3, selectElementOption4, selectElementOption5)
 
     const inputElement = document.createElement('input')
     inputElement.type = 'text'
--- API praktika/js/search.js
+++ API praktika/js/search.js
@@ -4,67 +4,140 @@ import createForm from './createForm.js'
 async function init() {
     const container = document.querySelector('#container')
     const searchResultWrap = document.createElement('div')
-    const pageTitle = createPageTitle('Search results:')
-    const ulElement = document.createElement('ul')
+    const pageTitle = createPageTitle('Search page')
+    const divElement = document.createElement('div')
     const searchPhrase = getUrlParams('search-phrase')
-    const formElement = document.querySelector('#form-element')
+    const formElement = createForm()
     
 
-    container.append(searchResultWrap)
-    searchResultWrap.append(createForm(), ulElement)
+    container.append(pageTitle, searchResultWrap)
+    searchResultWrap.append(formElement, divElement)
+    divElement.prepend(searchResults())
 
-formElement.addEventListener('submit', async function(e){
+    formElement.addEventListener('submit', async function(e){
 
-    e.preventDefault()
+        e.preventDefault()
 
-    const category = document.querySelector('#select').value
-    const input = document.querySelector('#search-phrase').value
+        const category = document.querySelector('#select').value
+        const input = document.querySelector('#search-phrase').value
 
-    if(category === 'posts'){
+        if(input){
 
-        const postsRes = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${searchPhrase}&_limit=20`)
-        const posts = await postsRes.json()
+            
+
+            if(category === 'posts'){
+
+                const postsRes = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${input}&_limit=20`)
+                const posts = await postsRes.json()
+
+                posts.forEach(data => {
+
+                    const { title } = data
+
+                    const parElement = document.createElement('p')
+                    parElement.textContent = `${title}`
+                    divElement.append(parElement)
+                });
+
+                if(posts.length === 0){
+                    divElement.append(dataNotFound('Posts not found'))
+                }     
+            }
+            
+
+            if(category === 'users'){
+
+                const usersRes = await fetch(`https://jsonplaceholder.typicode.com/users?q=${input}&_limit=20`)
+                const users = await usersRes.json()
+
+                users.forEach(data => {
+
+                    const {username, email} = data
+
+                    const parElement = document.createElement('p')
+                    parElement.textContent = `${username} ${email}`
+                    divElement.append(parElement)
+
+                })
+
+                if(users.length === 0){
+                    divElement.append(dataNotFound('User not found'))
+                }   
+            }
+
+            if(category === 'comments'){
+
+                const commentsRes = await fetch(`https://jsonplaceholder.typicode.com/comments?q=${input}&_limit=20`)
+                const comments = await commentsRes.json()
+
+                comments.forEach(data => {
+
+                    const { name, email, body } = data
+
+                    const parElement = document.createElement('p')
+                    parElement.textContent = `${name} ${email} ${body}`
+                    divElement.append(parElement)
+                })
+                if(comments.length === 0){
+                    divElement.append(dataNotFound('Commnets not found'))
+                }   
+            }
+                
+
+            if(category === 'albums'){
+
+                const albumsRes = await fetch(`https://jsonplaceholder.typicode.com/albums?q=${input}&_limit=20`)
+                const albums = await albumsRes.json()
+
+                albums.forEach(data => {
+
+                    const { title } = data
+
+                    const parElement = document.createElement('p')
+                    parElement.textContent = title
+                    divElement.append(parElement)
+                })
+
+                if(albums.length === 0){
+                    divElement.append(dataNotFound('Album not found'))
+                }   
+            }
 
 
+            if(category === 'photos'){
 
-        console.log(posts)
+                const photosRes = await fetch(`https://jsonplaceholder.typicode.com/photos?q=${input}&_limit=20`)
+                const photos = await photosRes.json()
 
-        posts.forEach(data => {
+                photos.forEach(data => {
 
-            const { title } = data
+                    const { title, url } = data
 
-            const liElement = document.createElement('li')
-            liElement.textContent = `${title}`;
-            ulElement.append(liElement);
-        });
-        return ulElement
-    }
+                    const photoTitle = document.createElement('p')
+                    photoTitle.textContent = `${title}:`
 
-    if(category === 'users'){
+                    const imgElement = document.createElement('img')
+                    imgElement.src = url
 
-        const usersRes = await fetch(`https://jsonplaceholder.typicode.com/users?q=${searchPhrase}&_limit=20`)
-        const users = await usersRes.json()
+                    divElement.append(photoTitle, imgElement)
+                })
 
-        users.forEach(data => {
+                if(photos.length === 0){
+                    divElement.append(dataNotFound('Photos not found'))
+                }   
+                
+            } 
 
-            const {username, email} = data
+            if(category === ''){
+                alert('Please choose category')
+            }
 
-            const liElement = document.createElement('li')
-            liElement.textContent = `${username} ${email}`
-            ulElement.append(liElement)
+            return divElement
+        }
 
-        });
-        return ulElement
-    }
-})
-
-    if (searchPhrase) {
-
-        console.log(posts)
-        console.log(users)
-    } else{
-        console.log('No search phrase...')
-    }
+            
+    })
+    
 }
 init()
 
@@ -74,6 +147,20 @@ function createPageTitle(text) {
     element.classList.add('page-title')
   
     return element
-  }
+}
 
+function searchResults() {
+    const searchResults = document.createElement('h2')
+    searchResults.textContent = 'Search results:'
+    searchResults.classList.add('page-title')
+  
+    return searchResults
+}
 
+function dataNotFound(text) {
+    const errorMessage = document.createElement('h1')
+    errorMessage.textContent = text
+    errorMessage.classList.add('page-title')
+  
+    return errorMessage
+}
